06acf7b831a70581bf8461b311ac761472e5f726
DurkaMateusz <mosca888@gmail.com>
2023-12-13 20:42:02 +0100

    added color switcher - works fine
    as tittle

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let colorSwitcher;
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+startBtn.addEventListener('click', () => {
+    if (!startBtn.hasAttribute('disabled')) {
+        colorSwitcher = setInterval(() => {
+            document.body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', true);
+    };
+});
+stopBtn.addEventListener('click', () => {
+    clearInterval(colorSwitcher);
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+});
+
 
